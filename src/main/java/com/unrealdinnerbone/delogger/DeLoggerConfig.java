ba941e0a0e57624d06de03abf835c91c72da1377
UnRealDinnerbone <unrealdinnerbone@gmail.com>
2021-08-23 08:34:25 -0500

    Fixed server crash

--- src/main/java/com/unrealdinnerbone/delogger/DeLoggerConfig.java
+++ src/main/java/com/unrealdinnerbone/delogger/DeLoggerConfig.java
@@ -1,20 +1,6 @@
 package com.unrealdinnerbone.delogger;
 
 import com.mojang.authlib.exceptions.AuthenticationUnavailableException;
-import com.mojang.authlib.yggdrasil.YggdrasilAuthenticationService;
-import net.minecraft.client.renderer.model.BlockModel;
-import net.minecraft.client.renderer.model.ModelBakery;
-import net.minecraft.client.renderer.texture.AtlasTexture;
-import net.minecraft.client.util.ClientRecipeBook;
-import net.minecraft.command.Commands;
-import net.minecraft.entity.EntityType;
-import net.minecraft.loot.LootTableManager;
-import net.minecraft.resources.SimpleReloadableResourceManager;
-import net.minecraft.util.Util;
-import net.minecraft.world.chunk.storage.ChunkSerializer;
-import net.minecraftforge.common.ForgeConfigSpec;
-import net.minecraftforge.fml.VersionChecker;
-import net.minecraftforge.registries.GameData;
 
 import java.util.Arrays;
 import java.util.Collections;
@@ -27,19 +13,19 @@ public class DeLoggerConfig {
     public boolean enableDebugLog = false;
     public List<String> whitelistLoggersTypes = Collections.singletonList("DebugFile");
     public List<String> loggers = Arrays.asList(
-            BlockModel.class.getCanonicalName(),
-            AtlasTexture.class.getCanonicalName(),
-            YggdrasilAuthenticationService.class.getCanonicalName(),
-            ModelBakery.class.getCanonicalName(),
-            ClientRecipeBook.class.getCanonicalName(),
-            Commands.class.getCanonicalName(),
-            Util.class.getCanonicalName(),
-            EntityType.class.getCanonicalName(),
-            LootTableManager.class.getCanonicalName(),
-            ChunkSerializer.class.getCanonicalName(),
-            GameData.class.getCanonicalName(),
-            ForgeConfigSpec.class.getCanonicalName(),
-            VersionChecker.class.getCanonicalName());
+            "net.minecraft.client.renderer.model.BlockModel",
+            "net.minecraft.client.renderer.texture.AtlasTexture",
+            "com.mojang.authlib.yggdrasil.YggdrasilAuthenticationService",
+            "net.minecraft.client.renderer.model.ModelBakery",
+            "net.minecraft.client.util.ClientRecipeBook",
+            "net.minecraft.command.Commands",
+            "net.minecraft.util.Util",
+            "net.minecraft.entity.EntityType",
+            "net.minecraft.loot.LootTableManager",
+            "net.minecraft.world.chunk.storage.ChunkSerializer",
+            "net.minecraftforge.registries.GameData",
+            "net.minecraftforge.common.ForgeConfigSpec",
+            "net.minecraftforge.fml.VersionChecker");
     public List<String> ignoredExceptions = Arrays.asList(
             AuthenticationUnavailableException.class.getCanonicalName()
     );
